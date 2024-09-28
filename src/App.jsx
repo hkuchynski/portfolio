386a495a749b26d0d6f91766c86dbf74da44cf89
Hanna Kuchynski <hannakuchynski@Hannas-MBP.fios-router.home>
2024-09-27 22:24:29 -0400

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@
  * @license Apache-2.0
  */
 
+/**
+ * Components
+ */
+import Header from "./components/Header";
+
 const App = () => {
 
-  return <h1>Hello</h1>
+  return (
+    <Header />
+  )
 
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+/**
+ * @copyright 2024 codewithsadee
+ * @license Apache-2.0
+ */
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar;
