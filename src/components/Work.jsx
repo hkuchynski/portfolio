7c0a652ef3a3eaabea4b9fda51d4bed494855b08
Hanna Kuchynski <hannakuchynski@Hannas-MBP.fios-router.home>
2024-12-03 14:23:59 -0500

    Added project to work page

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,28 +3,30 @@
  */
 import ProjectCard from "./ProjectCard";
 
+// tags: ['API', 'MVC', 'Development', 'SPA', 'Web-design', 'eCommerce']
+
 const works = [
     {
-      imgSrc: '/images/forkify.png',
-      title: 'Recipe Search App',
-      tags: ['Web-design', 'Development'],
-      projectLinks: ['https://github.com/hkuchynski/forkify', 'https://google.com/', '/images/forkify.gif'],
-      dataCaption: "Small recipe search app built with React, Redux, Redux Saga, Redux Persist. Design credit to Jonas Schmedtmann.",
+      imgSrc: '/images/ARCoreNav.png',
+      title: 'Indoor Navigation App',
+      tags: ['Development', 'ARCore', 'API'],
+      projectLinks: ['https://github.com/hkuchynski/Indoor-Navigation-ARCore', 'https://drive.google.com/file/d/1aOgRdCclyzSEfzeBDCU_U1xVcjSofOvg/view', '/images/ARCoreNav.png'],
+      dataCaption: "This is an Android navigation app built in Unity3D using Google ARCore. It features mapping, localization and navigation as part of assistive technology for BVI and ASD people. It was implemented with Google’s ARCore platform for building augmented reality experiences. The app uses A* Pathfinding algorithm to find the shortest path for the navigation. It facilitates the navigation by adding navigation voice commands. Utilized: Google ARCore, C#, Unity3D, NavMesh, Google Speech API",
       project: '1'
     },
     {
       imgSrc: '/images/therafolio.png',
       title: 'Massage Therapist Portfolio',
       tags: ['Web-design', 'Development'],
-      projectLinks: ['https://github.com/hkuchynski/therafolio', 'https://google.com/', '/images/therafolio.gif'],
+      projectLinks: ['https://github.com/hkuchynski/therafolio', 'https://hkuchynski.github.io/therafolio/', '/images/therafolio.gif'],
       dataCaption: "This is a one-page portfolio of a massage therapist built with HTML5 and Sass.",
       project: '2'
     },
     {
-      imgSrc: '/images/omnifood.png',
+      imgSrc: '/images/omnifood.jpg',
       title: 'Food Service Site',
       tags: ['Web-design', 'Development'],
-      projectLinks: ['https://github.com/hkuchynski/omnifood', 'https://google.com/', '/images/omnifood.gif'],
+      projectLinks: ['https://github.com/hkuchynski/omnifood', 'https://hkuchynski.github.io/omnifood/', '/images/omnifood.gif'],
       dataCaption: "This is a one-page landing site of a food service company build with HTML5 and CSS3.",
       project: '3'
     },
@@ -32,7 +34,7 @@ const works = [
       imgSrc: '/images/budgety.png',
       title: 'Budget App',
       tags: ['Web-design', 'Development'],
-      projectLinks: ['https://github.com/hkuchynski/budgety', 'https://google.com/', '/images/budgety.gif'],
+      projectLinks: ['https://github.com/hkuchynski/budgety', 'https://hkuchynski.github.io/budgety/', '/images/budgety.gif'],
       dataCaption: "A web app to manage your budget made with HTML, CSS, Javascript.",
       project: '4'
     }
